1acf5be6636a966fbab1bc774cf71de03cc15753
shekharr22 <shekharjadhav501@gmail.com>
2024-10-11 13:13:12 +0530

    initial commit

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { TodoProvider } from "./contexts";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [todos, setTodos] = useState([])
+ 
+ const addTodo = (todo) => { 
+  setTodos((prev) => [{id: Date.now()}, ...prev] ) 
+ }
+
+ const updatedTodo = (id, todo) => {
+  
+ }
 
   return (
-    <>
-     <h1> hello world </h1>
-    </>
-  )
+    <TodoProvider value={{todos, addTodo, updatedTodo, deleteTodo, toggleComplete}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+      <div className="w-full max-w-2xl px-4 py-3 mx-auto text-white rounded-lg shadow-md">
+        <h1 className="mt-2 mb-8 text-2xl font-bold text-center">
+          Manage Your Todos
+        </h1>
+        <div className="mb-4">{/* Todo form goes here */}</div>
+        <div className="flex flex-wrap gap-y-3">
+          {/*Loop and Add TodoItem here */}
+        </div>
+      </div>
+    </div>
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,24 +1,21 @@
 import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
-    todos:[
+    todos: [
         {
             id: 1,
             todo: "Todo msg",
             completed: false,
-             
-        }
-        
-        
-    ]
-})
+        },
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {},
+});
 
+export const useTodo = () => {
+    return useContext(TodoContext);
+};
 
-
-
-export const useTodo = () =>{
-    return useContext(TodoContext)
-}
-
-export const Todoprovider = TodoContext.Provider
-
+export const TodoProvider = TodoContext.Provider;
