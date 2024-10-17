b3295d052f35553d504fe00d31a1b6be34d5ce0b
shekharr22 <shekharjadhav501@gmail.com>
2024-10-17 18:51:08 +0530

    initial commit

--- reduxToolkit/src/app/store.js
+++ reduxToolkit/src/app/store.js
@@ -1,6 +1,10 @@
-import {configureStore} from '@reduxjs/toolkit';
-import todoReducer from '../features/todo/todoSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice'; // Import your slice
 
 export const store = configureStore({
-    reducer: todoReducer
-})
+  reducer: {
+    todo: todoReducer, // 'todo' is the key for your reducer
+  },
+});
+
+export default store;
