88d73a1f498abf6a50faf2e906ef0a85801ca3b1
shekharr22 <shekharjadhav501@gmail.com>
2024-10-19 17:19:37 +0530

    initial commit

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -2,6 +2,15 @@ import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
+       client = new Client();
+       account;
+
+       constructor(){
+          this.client
+          .setEndpoint(conf.appwriteUrl)
+          .setProject(conf.appwriteProjectId)
+        
+       }
 
 }
 
