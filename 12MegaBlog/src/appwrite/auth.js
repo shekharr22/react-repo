17d873d3b3f54afc21ea88ce69f2ebadf35fc16e
shekharr22 <shekharjadhav501@gmail.com>
2024-10-19 12:31:27 +0530

    initial commit

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,12 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+
+}
+
+    
+
+const AuthService = new AuthService();
+
+export default AuthService;
