edda6cd5bd187d20529e35f55e4857586e62f6c1
shekharr22 <shekharjadhav501@gmail.com>
2024-10-25 20:55:40 +0530

    initial commir

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
  console.log(import.meta.env.VITE_APPWRITE_URL);
- 
+
   return (
     <>
       <h1> max dev </h1>
@@ -12,3 +12,6 @@ function App() {
 }
 
 export default App
+
+
+
