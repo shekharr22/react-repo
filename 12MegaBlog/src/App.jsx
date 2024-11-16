b20522e845acc0a56048702883f67be1f74612d6
shekharr22 <shekharjadhav501@gmail.com>
2024-11-16 18:29:03 +0530

    initial commit

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,27 @@
-
+import React, { useState, useEffect} from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
 
 function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL);
+ const [loading, setloading] = useState(true)
+ const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1> max dev  </h1>
-    </>
+ useEffect(() => {
+     authService.getCurrentUser()
+     .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout({userData}))
+      }
+     })
+     .finally(() => setloading(false))
+ }, [])
+
+  return !loading ? (
+    <div className='flex flex-wrap content-between min-h-screen bg-gray-400'> max dev </div>
   )
 }
 
