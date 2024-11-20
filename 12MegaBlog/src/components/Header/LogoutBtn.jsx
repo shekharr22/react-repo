2a0e9f3a46aadb3ff65d385c581b8f7ab92bda17
shekharr22 <shekharjadhav501@gmail.com>
2024-11-20 17:03:59 +0530

    initial commit

--- 12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -6,12 +6,12 @@ import { logout } from '../../store/authSlice'
 function LogoutBtn() {
     const dispatch = useDispatch()
     const logoutHandler = () => {
-              
+      authService.logout().then(() => {
+        dispatch(logout())
+      })
     }
   return (
-    <div>
-      Loguout Button
-    </div>
+    <button className='inline-block px-6 py-2 duration-200 rounded-full hover:bg-blue-100'>Logout</button>
   )
 }
 
