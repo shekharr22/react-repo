d3361f5b3cc42418835de6638a90185b509e2a29
shekharr22 <shekharjadhav501@gmail.com>
2024-11-20 16:02:22 +0530

    initial commit

--- 12MegaBlog/src/components/container/Container.jsx
+++ 12MegaBlog/src/components/container/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children}) {
   return (
-    <div className='w-full px-4 mx-auto max-w-7xl'></div>
+    <div className='w-full px-4 mx-auto max-w-7xl'>{children}</div>
   )
 }
 
