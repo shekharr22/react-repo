ebe5947c3bb130b3eee7d50993ef7634c10e5d46
shekharr22 <shekharjadhav501@gmail.com>
2024-11-20 22:05:56 +0530

    initial commit

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
-import {Container, Logo, LogoutBtn} from '../index'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const authStatus = useSelector(state => state.auth.status);
+
+  const navigate = useNavigate();
+
+  const naItem = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus
+    }
+  ];
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header className="py-3 bg-gray-500 shadow ">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4"> 
+            <Link to='/'> </Link>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
