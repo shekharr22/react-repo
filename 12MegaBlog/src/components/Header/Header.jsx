afec55a3f945320ff87dc92a5a28be3f7232918f
shekharr22 <shekharjadhav501@gmail.com>
2024-11-21 20:48:54 +0530

    initial commit

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-  const authStatus = useSelector(state => state.auth.status);
+  const authStatus = useSelector((state) => state.auth.status);
 
   const navigate = useNavigate();
 
-  const naItem = [
+  const navItems = [
     {
       name: "Home",
       slug: "/",
@@ -40,9 +40,21 @@ function Header() {
     <header className="py-3 bg-gray-500 shadow ">
       <Container>
         <nav className="flex">
-          <div className="mr-4"> 
-            <Link to='/'> </Link>
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
           </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button onClick={() => navigate(item.slug)}
+                    className='inline-block px-6 py-2 duration-200 rounded-full hover:bg-blue-100'> {item.name} </button>
+                </li>
+              ) : null
+            )}
+          </ul>
         </nav>
       </Container>
     </header>
