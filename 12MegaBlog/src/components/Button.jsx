fbc986230049760e23f7ea0b7fe4800a4370c8d8
shekharr22 <shekharjadhav501@gmail.com>
2024-11-22 17:20:43 +0530

    initial commit

---
+++ 12MegaBlog/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Button({
+    children
+}) {
+  return (
+    <button>
+        {children}
+    </button>
+  )
+}
+
+export default Button
--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -6,14 +6,13 @@ import { useNavigate } from "react-router-dom";
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
-
   const navigate = useNavigate();
 
   const navItems = [
     {
       name: "Home",
       slug: "/",
-      active: true
+      active: true  
     },
     {
       name: "Login",
@@ -54,6 +53,11 @@ function Header() {
                 </li>
               ) : null
             )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            ) }
           </ul>
         </nav>
       </Container>
