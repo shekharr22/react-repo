104d1ddafb745247985c768f708225bc3b936da6
shekharr22 <shekharjadhav501@gmail.com>
2024-11-22 17:48:03 +0530

    initial commit

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -1,16 +1,18 @@
 import React, { useId } from "react";
 
-const Input = React.forwardRef(function Input({
-  label,
-  type = "text",
-  className = "",
-  ...props
-}, ref) {
-     return(
-      <div className='w-full'>
-        {label && <label className='block mb-1' htmlFor={props.id}>{label}</label>}
-      </div>
-     )
+const Input = React.forwardRef(function Input(
+  { label, type = "text", className = "", ...props },
+  ref
+) {
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="block pl-1 mb-1" htmlFor={props.id}>
+          {label}
+        </label>
+      )}
+    </div>
+  );
 });
 
 export default Input;
