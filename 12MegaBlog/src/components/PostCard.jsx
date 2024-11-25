da0a2fcb1de44611413d725e2a856739942326c4
shekharr22 <shekharjadhav501@gmail.com>
2024-11-25 19:33:33 +0530

    initial commit

--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import appwriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
 
-function PostCard() {
+function PostCard({ $id, title, featuredImage }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Link to={`/Post/${$id}`}>
+      <div className="w-full p-4 bg-gray-100 rounded-xl">
+        <div className="justify-center w-full mb-4">
+          <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+          className='rounded-xl' />
+          
+          </div>
+        <h2 className='text-xl font-bold'>{title}</h2>
+      </div>
+    </Link>
+  );
 }
 
-export default PostCard
+export default PostCard;
