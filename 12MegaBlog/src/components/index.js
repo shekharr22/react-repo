75b7333f7ebd58ab998835e49dc7b56a55692e53
shekharr22 <shekharjadhav501@gmail.com>
2024-11-27 17:14:34 +0530

    initial commit

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,13 +1,13 @@
-import Header from "./Header/Header"
-import Footer from "./Footer/Footer"
-import Container from "./container/Container"
-import Logo from "./Logo"
-import LogoutBtn from "./Header/LogoutBtn"
-
-export {
-    Header,
-    Footer,
-    Container,
-    Logo,
-    LogoutBtn
-}
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+export { Header,
+         Footer,
+         Container,
+         Logo,
+         LogoutBtn,
+         RTE 
+};
--- 12MegaBlog/src/components/post-form/PostForm.jsx
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, Select, RTE } from "../index";
+import appwriteService from "../../appwrite/config";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-function PostForm() {
-  return (
-    <div>
-      
-    </div>
-  )
+function PostForm({ post }) {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active"
+      }
+    });
+
+  const navigate = useNavigate();
+  const userData = useSelector(state => state.user.userData)
+  const submit = async (data) => {
+    if(post) {
+        
+    }
+  }
+   
+  return <div>PostForm</div>;
 }
 
-export default PostForm
+export default PostForm;
